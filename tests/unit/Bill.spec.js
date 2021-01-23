5707b33db4e92a4a7ab6191f75ad741e19cf3129
EMRE <1907burem@gmail.com>
2021-01-23 21:29:57 +0300

    Merge branch 'master' of https://github.com/emrecck/Nuxt-Jest-Testing

--- tests/unit/Bill.spec.js
+++ tests/unit/Bill.spec.js
@@ -3,7 +3,14 @@ import Bill from '@/components/Bill.vue'
 
 describe('Cart', () => {
   test('is a Vue instance', () => {
-    const wrapper = mount(Bill)
+    const wrapper = mount(Bill, {
+
+      stubs: {
+        NuxtLink: true,
+        // Any other component that you want stubbed
+      },
+    }
+      )
     expect(wrapper.vm).toBeTruthy()
   })
 })
